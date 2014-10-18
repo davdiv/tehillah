1f32cb0f1f2e146d272f76468910cb1a455bd4c9
DivDE <divde@free.fr>
2014-10-18 16:41:15 +0200

    Updating hashspace

--- preinstall.sh
+++ preinstall.sh
@@ -2,7 +2,7 @@
 
 if ! [ -d node_modules/hashspace ] ; then
    mkdir -p node_modules &&
-   git clone https://github.com/jakub-g/hashspace.git --branch=htmlsyntax-part3 node_modules/hashspace --depth 1 &&
+   git clone https://github.com/divdavem/hashspace.git node_modules/hashspace --depth 1 &&
    (  cd node_modules/hashspace && 
       npm install grunt-cli && 
       npm_config_production=false npm install &&
